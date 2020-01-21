c88af4091077025470ea57fd4873822145a62a8d
Evan Hum <54876464+e2hum@users.noreply.github.com>
2020-01-21 13:27:12 -0500

    updated header file

--- A1/polynomial.h
+++ A1/polynomial.h
@@ -1,4 +1,14 @@
+#ifndef polynomial
+#define polynomial
+
 class Polynomial
 {
+    // private attributes
+    int* data;
+    int data_size;
 
+    public:
+        Polynomial(int A[], int size);
+        ~Polynomial();
 };
+#endif
